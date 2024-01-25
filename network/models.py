4131dc669713c58312d6305a098fad8a9d13b705
Vahid-Kosari <vahid59m@yahoo.com>
2024-01-25 14:41:59 +0330

    Models finalized, Title changed to Poster

--- network/models.py
+++ network/models.py
@@ -1,6 +1,27 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import datetime
 
 
 class User(AbstractUser):
     pass
+
+
+# If a user is followed by another ones, it means that those users are following this user.
+class FollowState(models.Model):
+    user: models.ForeignKey(User, on_delete=models.CASCADE, related_name="the_user")
+    followed_by: models.ManyToManyField(User, blank=True, related_name="followed_by")
+
+    def __str__(self):
+        return f"{self.user} is followed BY {self.followed_by}."
+
+
+# A post has just one author and multiple likes from different users, if any. So, M2M field and blank:True
+class Post(models.Model):
+    author: models.ForeignKey(User, on_delete=models.CASCADE)
+    content: models.TextField(blank=False)  # Charfield(max_lenght=255)
+    likes: models.ManyToManyField(User, blank=True, related_name="likes")
+    creation_date: models.DateTimeField(default=datetime.now())  # auto_now_add=True
+
+    def __str__(self):
+        return f"{self.author} posted a post."
