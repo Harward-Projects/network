ff0076ec06e6634804d40d24ca1a7a902c595eb4
Vahid-Kosari <vahid59m@yahoo.com>
2024-01-29 23:34:41 +0330

    New post form created and JS reference added
    1- default submit action prevented through index.js
    2- post function should be written in views.py
    3- appropriate url.py path should be considered
    4- appropriate index.js function should be written

---
+++ network/static/network/index.js
@@ -0,0 +1,7 @@
+const submitPostButton = document.getElementById('submitPost');
+
+submitPostButton.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent the default submit action
+
+  // Perform any custom logic here, such as validation or custom form submission
+});
