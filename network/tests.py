96ffcb926aa85f584bc9cb1f8460f65943b31764
Vahid-Kosari <vahid59m@yahoo.com>
2024-04-14 02:35:27 +0330

    Tests started by selenium and unittest, pytest will be next. Database separation unsuccessful

--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,56 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from selenium import webdriver
 
-# Create your tests here.
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# from django.contrib.auth.models import User
+
+# db.connection.set_schema("test")
+
+
+class FunctionalTestClass(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def test_01(self):
+        self.browser.get("http://127.0.0.1:8000/")
+        self.browser.find_element(By.LINK_TEXT, "Log In").click()
+        username = self.browser.find_element(By.NAME, "username")
+        username.send_keys("vahid59m")
+        password = self.browser.find_element(By.NAME, "password")
+        password.send_keys("vahid59m")
+        self.browser.find_element("css selector", ".btn.btn-primary").click()
+
+        wait = WebDriverWait(self.browser, 10)  # Wait up to 10 seconds
+        post = wait.until(EC.presence_of_element_located((By.ID, "id_content")))
+        # post = self.browser.find_element(By.ID, "id_content")
+        post.send_keys("The New Test Text")
+        self.browser.find_element("id", "submitPost").click()
+        self.assertIn("Test text", self.browser.page_source)
+
+    def tearDown(self):
+        time.sleep(10)
+        self.browser.quit()
+
+
+# class UnitTestClass(TestCase):
+
+#     def setUp(self):
+#         pass
+
+#     def test_assess_response_from_index_url(self):
+#         user = User.objects.create_user(
+#             username="tester01", email="tester01@example.com", password="password"
+#         )
+#         client = Client()
+#         response = client.get("/")
+#         self.assertEqual(response.status_code, 200)
+
+
+# if __name__ == "__main__":
+#     unittest.main()
--- project4/settings.py
+++ project4/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import os, sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,8 +79,19 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    },
+    "test": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "test_db.sqlite3"),
+    },
 }
+
+# Specify test database usage
+if "test" in sys.argv:
+    DATABASES["default"] = DATABASES["test"]
+
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 AUTH_USER_MODEL = "network.User"
 
